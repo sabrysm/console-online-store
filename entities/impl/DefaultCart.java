e99b1fddd1c3bad42d614f7c43394229996cd5a1
Abdelrahman Sabry <Drakth5364@gmail.com>
2025-09-08 23:08:39 +0300

    feat: add implementation for default cart

--- entities/impl/DefaultCart.java
+++ entities/impl/DefaultCart.java
@@ -3,32 +3,55 @@ package oop.project.onlineshop.entities.impl;
 import oop.project.onlineshop.entities.Cart;
 import oop.project.onlineshop.entities.Product;
 
+import java.util.Arrays;
+
 public class DefaultCart implements Cart {
 
-    // <write your code here>
+    private Product[] products;
+    private int size;
+    private static final int DEFAULT_CART_SIZE = 10;
+
+    {
+        products = new Product[DEFAULT_CART_SIZE];
+        size = 0;
+    }
 
     @Override
     public boolean isEmpty() {
-        // <write your code here>
-
-        return true;
+        return size == 0;
     }
 
     @Override
     public void addProduct(Product product) {
-        // <write your code here>
+        if (size + 1 == products.length) grow();
+        products[size++] = product;
     }
 
     @Override
     public Product[] getProducts() {
-        // <write your code here>
+        int actualSize = 0;
+        for (Product p : products) {
+            if (p != null) actualSize++;
+        }
+        Product[] nonNullProducts = new Product[actualSize];
+        int counter = 0;
+        for (Product p : products) {
+            if (p != null) {
+                nonNullProducts[counter++] = p;
+            }
+        }
 
-        return null;
+        return nonNullProducts;
     }
 
     @Override
     public void clear() {
-        // <write your code here>
+        products = new Product[0];
+        size = 0;
     }
 
+    private void grow() {
+        int newLength = size + size >> 1; // increase capacity by 50%
+        products = Arrays.copyOf(products, newLength);
+    }
 }
